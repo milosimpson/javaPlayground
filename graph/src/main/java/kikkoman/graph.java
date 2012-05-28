b058cc824c4b941e8a5ed98bcbcfd9ba5d36c255
Milo Simpson <milo.simpson@gmail.com>
2012-05-28 14:06:43 -0500

    Model relationships and correctly identifies levels. Walks the graph instead of using the search APIs.

--- graph/src/main/java/kikkoman/graph.java
+++ graph/src/main/java/kikkoman/graph.java
@@ -43,12 +43,12 @@ public class graph {
     DEPENDS
   }
 
-  private static Node createAndIndexPropertyName( final String propertyName, final int ruleNumber )
+  private static Node createAndIndexPropertyName( final String propertyName )
   {
     Node propertyNode = nodeIndex.get( PROPERTY_NAME, propertyName).getSingle();
 
     if ( propertyNode != null ) {
-      System.out.println( "Found propertyName " + propertyName );
+      System.out.println( "   Found pre-existing node with propertyName " + propertyName );
       return propertyNode;
     }
 
@@ -56,7 +56,6 @@ public class graph {
     node.setProperty( PROPERTY_NAME, propertyName );
     nodeIndex.add( node, PROPERTY_NAME, propertyName );
 
-    node.setProperty( "ruleNumber", ruleNumber );
     return node;
   }
 
@@ -68,38 +67,51 @@ public class graph {
     registerShutdownHook();
     // END SNIPPET: startDb
 
-    List<Set<String>> rulesFrom = new ArrayList<Set<String>>(6);
-    List<Set<String>> rulesTo   = new ArrayList<Set<String>>(6);
+    List<Set<String>> rulesFrom = new ArrayList<Set<String>>(7);
+    List<Set<String>> rulesTo   = new ArrayList<Set<String>>(7);
+    List<String> rulesStrings   = new ArrayList<String>(7);
 
     rulesFrom.add(0, new HashSet<String>() );
     rulesFrom.get(0).addAll( Arrays.<String>asList( "citem.clientid", "citem.type", "citem.externalid" ) );
     rulesTo.add(0, new HashSet<String>() );
     rulesTo.get(0).addAll( Arrays.<String>asList( "citem.id" ) );
+    rulesStrings.add(0, "citem.clientid + citem.type + citem.externalid -> citem.id");
 
     rulesFrom.add(1, new HashSet<String>() );
-    rulesFrom.get(1).addAll( Arrays.<String>asList("citem.parents") );
+    rulesFrom.get(1).addAll( Arrays.<String>asList("citem[type=product].parents") );
     rulesTo.add(1, new HashSet<String>() );
-    rulesTo.get(1).addAll( Arrays.<String>asList("citem[type=product].ancestorSet", "citem[type=category].ancestorSet") );
+    rulesTo.get(1).addAll( Arrays.<String>asList("citem[type=product].ancestorSet") );
+    rulesStrings.add(1, "walk(citem[type=product].parents) -> citem[type=product].ancestorSet");
 
     rulesFrom.add(2, new HashSet<String>() );
-    rulesFrom.get(2).addAll( Arrays.<String>asList("citem[type=product].ancestorSet") );
+    rulesFrom.get(2).addAll( Arrays.<String>asList("citem[type=category].parents") );
     rulesTo.add(2, new HashSet<String>() );
-    rulesTo.get(2).addAll( Arrays.<String>asList("citem[type=category].numProducts") );
+    rulesTo.get(2).addAll( Arrays.<String>asList( "citem[type=category].ancestorSet") );
+    rulesStrings.add(2, "walk(citem[type=category].parents) -> citem[type=category].ancestorSet");
 
     rulesFrom.add(3, new HashSet<String>() );
-    rulesFrom.get(3).addAll( Arrays.<String>asList("review.syn_about") );
+    rulesFrom.get(3).addAll( Arrays.<String>asList("citem[type=product].ancestorSet") );
     rulesTo.add(3, new HashSet<String>() );
-    rulesTo.get(3).addAll( Arrays.<String>asList("citem[type=product].numReviews") );
+    rulesTo.get(3).addAll( Arrays.<String>asList("citem[type=category].numProducts") );
+    rulesStrings.add(3, "count( citem[type=product].ancestorSet ) -> citem[type=category].numProducts");
 
     rulesFrom.add(4, new HashSet<String>() );
-    rulesFrom.get(4).addAll( Arrays.<String>asList("review.syn_about", "review.rating") );
+    rulesFrom.get(4).addAll( Arrays.<String>asList("review.syn_about") );
     rulesTo.add(4, new HashSet<String>() );
-    rulesTo.get(4).addAll( Arrays.<String>asList("citem[type=product].avgRating") );
+    rulesTo.get(4).addAll( Arrays.<String>asList("citem[type=product].numReviews") );
+    rulesStrings.add(4, "count( review.syn_about ) -> citem[type=product].numReviews");
 
     rulesFrom.add(5, new HashSet<String>() );
-    rulesFrom.get(5).addAll( Arrays.<String>asList("citem[type=product].ancestorSet", "citem[type=product].numReviews") );
-    rulesTo.add(5, new HashSet<String>());
-    rulesTo.get(5).addAll( Arrays.<String>asList("citem[type=category].avgRating") );
+    rulesFrom.get(5).addAll( Arrays.<String>asList("review.syn_about", "review.rating") );
+    rulesTo.add(5, new HashSet<String>() );
+    rulesTo.get(5).addAll( Arrays.<String>asList("citem[type=product].avgRating") );
+    rulesStrings.add(5, "sum( review.rating ) / count( review.syn_about ) -> citem[type=product].avgRating");
+
+    rulesFrom.add(6, new HashSet<String>() );
+    rulesFrom.get(6).addAll( Arrays.<String>asList("citem[type=product].numReviews", "citem[type=product].avgRating") );
+    rulesTo.add(6, new HashSet<String>());
+    rulesTo.get(6).addAll( Arrays.<String>asList("citem[type=category].avgRating") );
+    rulesStrings.add(6, "sum( citem[type=product].avgRating * citem[type=product].numReviews ) / sum( citem[type=product].numReviews ) -> citem[type=category].avgRating");
 
     Set<Node> allNodes = new HashSet<Node>();
     Set<Relationship> allRelationships = new HashSet<Relationship>();
@@ -110,27 +122,31 @@ public class graph {
     {
       for( int ruleNumber = 0; ruleNumber < rulesTo.size(); ruleNumber++ ) {
 
+        System.out.println( "Adding nodes and rule #" + ruleNumber + " to the graph: " + rulesStrings.get(ruleNumber) );
+
         Set<String> from = rulesFrom.get(ruleNumber);
 
         Set<Node> toNodes   = new HashSet<Node>();
         Set<String> to   = rulesTo.get(ruleNumber);
 
         for( String toProperty : to ) {
-          Node node = createAndIndexPropertyName(toProperty, ruleNumber);
+          Node node = createAndIndexPropertyName( toProperty );
           toNodes.add( node );
           allNodes.add( node );
         }
 
         for( String fromProperty : from ) {
-          Node fromNode = createAndIndexPropertyName( fromProperty, ruleNumber);
-
-//          System.out.println( "fromNode" + " : propertyName=" + fromNode.getProperty( PROPERTY_NAME )
-//              + " rule number=" +  fromNode.getProperty( "ruleNumber" ) );
+          Node fromNode = createAndIndexPropertyName( fromProperty );
 
           allNodes.add( fromNode );
 
           for( Node toNode : toNodes ) {
-            allRelationships.add(toNode.createRelationshipTo(fromNode, RelTypes.DEPENDS));
+            Relationship rel = fromNode.createRelationshipTo(toNode, RelTypes.DEPENDS);
+
+            rel.setProperty( "ruleString", rulesStrings.get(ruleNumber) );
+            rel.setProperty( "ruleNumber", ruleNumber );
+
+            allRelationships.add(rel);
           }
         }
       }
@@ -150,6 +166,7 @@ public class graph {
     while( foundSome ) {
       foundSome = false;
 
+      // Find levels of calculation, by finding roots, removing them, and then making subsequent passes
       tx = graphDb.beginTx();
       try
       {
@@ -160,9 +177,6 @@ public class graph {
 
           if ( ! node.hasRelationship( Direction.INCOMING ) && node.hasProperty( PROPERTY_NAME ) ) {
 
-            System.out.println( "Level = " + level + " : propertyName = " + node.getProperty( PROPERTY_NAME )
-                + " ruleNumber = " +  node.getProperty( "ruleNumber" ) );
-
             toDelete.add( node );
 
             foundSome = true;
@@ -171,7 +185,12 @@ public class graph {
 
         for( Node node : toDelete ) {
           Iterable<Relationship> outgoingRels = node.getRelationships( Direction.OUTGOING );
+
+          System.out.println( "Level = " + level + " property = " + node.getProperty( PROPERTY_NAME ) );
+
           for( Relationship rel : outgoingRels ) {
+            System.out.println( " Clears Rule #" + rel.getProperty( "ruleNumber" ) + " " +  rel.getProperty( "ruleString" ) );
+
             rel.delete();
           }
 
