ae124059f26561b290b47255dc7f50441fdf920d
Milo Simpson <milo.simpson+github@gmail.com>
2020-08-03 18:53:28 -0500

    Cool, can run the same test twice in by having two suites, with static param.

--- parityTesting/src/test/java/org/kikkoman/parity/FrontendTest.java
+++ parityTesting/src/test/java/org/kikkoman/parity/FrontendTest.java
@@ -4,15 +4,29 @@ package org.kikkoman.parity;
 import static org.testng.Assert.assertTrue;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class FrontendTest {
 
   private Frontend frontend;
 
+  @Parameters({ "backendType" })
   @BeforeClass
-  public void setup() {
-    Backend backend = new BackendA();
+  public void setup(@Optional("A") String backendType) {
+    Backend backend;
+
+    if ("A".equals(backendType)) {
+      backend = new BackendA();
+    }
+    else if ("B".equals(backendType)) {
+      backend = new BackendB();
+    }
+    else {
+      throw new RuntimeException("Unknown Backend specified:" + backendType);
+    }
+
     frontend = new Frontend(backend);
   }
 
